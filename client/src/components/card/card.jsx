59407f55eeff9d46b2ad2af149e01391bc8ce41f
Milthon Rodriguez <milthon@gmail.com>
2023-04-09 22:47:55 -0500

    2

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -5,15 +5,7 @@ const Card = ({ id, image, name, temperaments }) => {
   const temper = temperaments.map((t) => t + ", ");
   console.log(temper);
   return (
-    // <div className={style.caja}>
-    //   <Link to={`/detail/${id}`}>
-    //     <h3 className={style.name}>{name}</h3>
-
-    //     <img src={image} alt={name} className={style.image} />
-    //   </Link>
-    //   <div className={style.titulo}>Temperaments</div>
-    //   <h4 className={style.temper}>{temper}</h4>
-    // </div>
+    
     <div className={style.card}>
       <div className={style.detaills}>
         <Link to={`/detail/${id}`}>
@@ -24,7 +16,7 @@ const Card = ({ id, image, name, temperaments }) => {
           <li><strong>Temperamentos:</strong> {temper}</li>
         </ul>
       </div>
-    </div >
+    </div>
 
 
   );
